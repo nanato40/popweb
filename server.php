f0948f2cedb0d2228a394a9c7c607929d8640c70
renato <renatochaves91@gmail.com>
2018-10-07 03:17:25 -0300

    ArquivoServerModificado

--- server.php
+++ server.php
@@ -18,4 +18,15 @@ if ($uri !== '/' && file_exists(__DIR__.'/public'.$uri)) {
     return false;
 }
 
-require_once __DIR__.'/public/index.php';
+require_once __DIR__.'/public/index.php'
+
+
+
+
+
+
+
+
+
+
+;
